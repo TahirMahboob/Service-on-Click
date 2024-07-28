ad5244b39b0fa990e0ab2d035abbb1ed06e051d5
TahirMahboob <tahirmehboob06@gmail.com>
2024-07-28 19:24:01 +0500

    F+B(node)

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -2,9 +2,12 @@ const loginController = require('../controller/login.js');
 const express = require('express');
 const { auth } = require('../middlewares/auth.js');
 const router = express.Router();
+
 router.post('/login', loginController.login);
+
 router.post('/logOut', loginController.logOut);
 router.post('/forgot', loginController.forgotPasswordService);
 router.post('/reset', loginController.resetPassword);
+
 router.post('/changePassword', auth('admin'), loginController.resetPassword);
 module.exports = router;
--- Backend/routes/signup.js
+++ Backend/routes/signup.js
@@ -3,5 +3,6 @@ const signupController=require("../controller/signup.js")
 const express=require("express")
 const router=express.Router()
 router.post("/register",signupController.createUser)
+
 module.exports=router
  
